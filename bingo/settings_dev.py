7f6c6eeeec339a727d655f41275cf529cbec9c3d
Jay Hale <jay@jtst.io>
2017-11-07 23:28:03 -0500

    Sorting out settings

---
+++ bingo/settings_dev.py
@@ -0,0 +1,5 @@
+from .settings_commoon import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
--- cards/models.py
+++ cards/models.py
@@ -1,3 +1,27 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
+
+
+class Card(models.Model):
+    user = models.ForeignKey(AUTH_USER_MODEL, ondelete=models.CASCADE)
+    order = models.CharField(max_length=128)
+
+    def __str__(self):
+        return str(self.user)
+
+
+class Week(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+
+class Square(models.Model):
+    week = models.ForeignKey(Week)
+    content = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.content
